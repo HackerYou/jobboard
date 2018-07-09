117b1f3d90494db73bfd8a2d1437b4cc94203107
Zoe Daniels <zkdaniels@gmail.com>
2018-07-09 11:07:24 -0400

    conflix

--- dev/scripts/components/MyPostedJobs.js
+++ dev/scripts/components/MyPostedJobs.js
@@ -30,56 +30,52 @@ class MyPostedJobs extends React.Component {
 
     render() {
         return (
-        <div className="job-feed-container job-feed-container--my-posted">
-<<<<<<< HEAD
-=======
-            <p>posted jobs</p>
->>>>>>> 7a98bc7b2e33629b9f1373729e724d29f35ce3cf
-            <div className="job-feed">
-                {this.state.postedJobs && Object.keys(this.state.postedJobs)
-                .filter(jobId => this.state.postedJobs[jobId].archived === false)
-                .map(jobId => {
-                let job = this.state.postedJobs[jobId];
-                    if(job.archived === false){
-                        return (
-                        <JobPreview 
-                        showJobDetails={this.showJobDetails} 
-                        saveJob={this.saveJob} 
-                        key={jobId} 
-                        companyName={job.companyName} 
-                        jobTitle={job.jobTitle} 
-                        jobLocation={job.jobLocation} 
-                        jobDescription={job.jobDescription} 
-                        datePosted={job.timeCreated} 
-                        jobId={jobId} 
-                        archived={job.archived} 
-                        approved={job.approved} 
-                        userId={this.props.userId} 
-                        showArchive={true} 
-                        active={this.state.showingJobId === jobId ? 'active': null}
-                        alumni={this.props.alumni}
-                        admin={this.props.admin}
-                        addressee={this.props.addressee}
-                        jobPoster={this.props.jobPoster}
+            <div className="job-feed-container job-feed-container--my-posted">
+                <div className="job-feed">
+                    {this.state.postedJobs && Object.keys(this.state.postedJobs)
+                    .filter(jobId => this.state.postedJobs[jobId].archived === false)
+                    .map(jobId => {
+                    let job = this.state.postedJobs[jobId];
+                        if(job.archived === false){
+                            return (
+                            <JobPreview 
+                            showJobDetails={this.showJobDetails} 
+                            saveJob={this.saveJob} 
+                            key={jobId} 
+                            companyName={job.companyName} 
+                            jobTitle={job.jobTitle} 
+                            jobLocation={job.jobLocation} 
+                            jobDescription={job.jobDescription} 
+                            datePosted={job.timeCreated} 
+                            jobId={jobId} 
+                            archived={job.archived} 
+                            approved={job.approved} 
+                            userId={this.props.userId} 
+                            showArchive={true} 
+                            active={this.state.showingJobId === jobId ? 'active': null}
+                            alumni={this.props.alumni}
+                            admin={this.props.admin}
+                            addressee={this.props.addressee}
+                            jobPoster={this.props.jobPoster}
 
-                        />);
-                    } 
-                })}
-            </div>
-            {this.state.showDetails && <FullJob 
-                        jobId={this.state.showingJobId} 
-                        jobTitle={this.state.postedJobs[`${this.state.showingJobId}`]['jobTitle']}
-                        jobLocation={this.state.postedJobs[`${this.state.showingJobId}`]['jobLocation']}
-                        jobDescription={this.state.postedJobs[`${this.state.showingJobId}`]['jobDescription']}
-                        companyName={this.state.postedJobs[`${this.state.showingJobId}`]['companyName']}
-                        datePosted={this.state.postedJobs[`${this.state.showingJobId}`]['datePosted']}
-                        approved={this.state.postedJobs[`${this.state.showingJobId}`]['approved']}
-                        jobCommitment={this.state.postedJobs[`${this.state.showingJobId}`]['jobCommitment']}
-                        addressee={this.state.postedJobs[`${this.state.showingJobId}`]['addressee']}
+                            />);
+                        } 
+                    })}
+                </div>
+                {this.state.showDetails && <FullJob 
+                            jobId={this.state.showingJobId} 
+                            jobTitle={this.state.postedJobs[`${this.state.showingJobId}`]['jobTitle']}
+                            jobLocation={this.state.postedJobs[`${this.state.showingJobId}`]['jobLocation']}
+                            jobDescription={this.state.postedJobs[`${this.state.showingJobId}`]['jobDescription']}
+                            companyName={this.state.postedJobs[`${this.state.showingJobId}`]['companyName']}
+                            datePosted={this.state.postedJobs[`${this.state.showingJobId}`]['datePosted']}
+                            approved={this.state.postedJobs[`${this.state.showingJobId}`]['approved']}
+                            jobCommitment={this.state.postedJobs[`${this.state.showingJobId}`]['jobCommitment']}
+                            addressee={this.state.postedJobs[`${this.state.showingJobId}`]['addressee']}
 
-             />}
+                />}
            
-          </div>)
+            </div>)
     }
 }
 export default MyPostedJobs
